056a5ab1cc4df83ed6003a4fe81f7149302b6f77
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-06-24 21:54:48 -0700

    Adding jscript.

--- liri.js
+++ liri.js
@@ -9,3 +9,30 @@ var spotify = new Spotify(keys.spotify);
 
 //enable axios
 var axios = require("axios");
+
+//variable for user entry
+
+var input = process.argv.slice(2).join(' ')
+
+//Switch statement for various LIRI functions
+switch (input) {
+
+case "concert-this":
+    console.log("concert-this function");
+    break;
+
+case "spotify-this-song":
+    console.log("spotify-this-song function");
+    break;
+
+case "movie-this":
+    console.log("movie-this function");
+    break;
+
+case "do-what-it-says":
+    console.log("do-what-it-says function");
+    break;
+
+
+
+}
