909a9228a7b1b7c84c753fbdfec5389d653a0c25
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-06-25 23:32:06 -0700

    Added functions to js file.

--- liri.js
+++ liri.js
@@ -17,7 +17,7 @@ var axios = require("axios");
 //variables for user command and search term entry
 
 var command = process.argv[2];
-var input = process.argv.slice(3).join('+');
+var input = process.argv.slice(3).join(' ');
 
 
 
@@ -25,12 +25,13 @@ var input = process.argv.slice(3).join('+');
 switch (command) {
 
 case "concert-this":
-    console.log("concert-this function");
+    // console.log("concert-this function");
     concertThis();
     break;
 
 case "spotify-this-song":
-    console.log("spotify-this-song function");
+    // console.log("spotify-this-song function");
+    spotifyThisSong();
     break;
 
 case "movie-this":
@@ -54,14 +55,55 @@ function concertThis () {
 var concertQuery = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp"
 
 axios.get(concertQuery).then(
-    function(response){
-     for (var i = 0; i <= response.length; i++);
+    function (response){
+       
+    // Get function to correctly list each instance of logged data
+     for (var i = 0; i < response.data.length; i++){
+     console.log("================================="); 
      console.log("Venue: " + response.data[i].venue.name); 
      console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
-     console.log("Date: " + response.data[i].datetime);
+
+    //TODO: Get moment js to display the time correctly  - below doesn't work
+    // console.log("Date: " + moment(response.data[i].datetime, 'YYYY-MM-DDTHH:mm:ss').format('MM/DD/YYY, h:mm A')); 
+
+     console.log("Date: " + response.data[i].datetime); }
+
+
     }
 );
 
 
 
 };
+
+
+//spotify-this-song
+
+function spotifyThisSong(){
+
+    spotify.search({ type: 'track', query: input , limit: 5
+    }).then(function(response){
+     console.log("Artist(s): " + response.tracks.items[0].artists[0].name);
+     console.log("Song Title: " + response.tracks.items[0].name);
+     console.log("Preview Link: " + response.tracks.items[0].preview_url);
+     console.log("Album: " + response.tracks.items[0].album.name);
+
+
+    });
+
+
+};
+
+
+//movie-this 
+
+function movieThis(){
+
+
+
+
+
+
+
+    
+}
